5423b6baa0898580ea5cbbd10469bbd3c4996352
Mustafa Elgmal <mostafaelgmal36@gmail.com>
2022-10-22 16:52:03 +0200

    remove auth from tags

--- src/routers/tag.ts
+++ src/routers/tag.ts
@@ -22,7 +22,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/",auth,async (req, res) => {
+router.get("/",async (req, res) => {
   try {
     const tags = await Tag.find();
     res.json({ tags });
